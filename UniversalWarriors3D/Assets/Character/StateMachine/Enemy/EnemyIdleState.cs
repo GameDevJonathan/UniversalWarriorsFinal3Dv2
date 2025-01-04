0afbf89140781ad9e9ff91619c803a987d4c8ba5
GameDevJonathan <jfagan16@gmail.com>
2025-01-04 13:44:42 -0500

    enemy idle state

---
+++ UniversalWarriors3D/Assets/Character/StateMachine/Enemy/EnemyIdleState.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+
+///In this state Enemies just stand around doing nothing. 
+///Idly looking around and playing a random animation after a while. 
+///If in this state or in the patrol state if the enemy will enter the chase 
+///state. 
+
+public class EnemyIdleState : EnemyBaseState
+{
+    private readonly int FreeLookHash = Animator.StringToHash("Locomotion");
+    private readonly int SpeedHash = Animator.StringToHash("Speed");
+    private const float crossFadeTime = 0.1f;
+    private const float AnimatorDampTime = 0.1f;
+    private float RandomIdleTimer;
+    
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine){}
+
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(FreeLookHash, crossFadeTime);
+        RandomIdleTimer = Random.Range(stateMachine.IdleRangeTimer.x, stateMachine.IdleRangeTimer.y);
+        
+    }
+    public override void Tick(float deltaTime)
+    {
+        if (RandomIdleTimer > 0)
+        {
+            //RandomIdleTimer = Mathf.Max(RandomIdleTimer - Time.deltaTime, 0);
+        }
+        else
+        {
+            //ToDo Trigger Random Idle Animation
+        }
+
+        stateMachine.Animator.SetFloat(SpeedHash, 0, AnimatorDampTime, deltaTime );
+        
+    }
+
+    public override void Exit(){}
+
+}
--- UniversalWarriors3D/Assets/Character/StateMachine/Enemy/EnemyStateMachine.cs
+++ UniversalWarriors3D/Assets/Character/StateMachine/Enemy/EnemyStateMachine.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
+using Unity.Cinemachine;
+ 
 
 public class EnemyStateMachine : StateMachine
 {
-    
+    [field: SerializeField] public Animator Animator { get; private set; }
+
+    [MinMaxRangeSlider(0,100)]
+    [Tooltip("Idle Timer to trigger random idle animation")]
+    [field:SerializeField] public Vector2 IdleRangeTimer { get; private set; }
+
+
+    private void Start()
+    {
+        SwitchState(new EnemyIdleState(this));
+    }
+
 }
